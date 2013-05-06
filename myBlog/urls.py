f719ce80e1474a4d6011608d58b21112deba6b45
regia <regedi@gmail.com>
2013-05-06 11:25:44 +0300

    First commit

---
+++ myBlog/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+                       # Examples:
+                       # url(r'^$', 'myBlog.views.home', name='home'),
+                       # url(r'^myBlog/', include('myBlog.foo.urls')),
+
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^tinymce/', include('tinymce.urls')),
+                       url(r'^blog/', include('blog.urls')),
+)
