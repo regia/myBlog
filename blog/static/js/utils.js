8bdc78e211e8fae422155fafd7bbcb42a5e59f7b
regia <regedi@gmail.com>
2013-05-27 11:03:19 +0300

    added event when press enter on input field

--- blog/static/js/utils.js
+++ blog/static/js/utils.js
@@ -1,22 +1,6 @@
 $(document).ready(function () {
 
-    //use on sign in button to show login form
-    $('.login').on('click', function () {
-        $('.loginform').fadeIn("slow", "linear");
-        $(this).hide();
-    });
-
-
-    //use on login cancel button
-    $('.login_cancel').on("click", function (e) {
-        //e.preventDefault();
-        $('.login_errors').hide();
-        $('.loginform').fadeOut(500);
-        $('.login').show(1000);
-    });
-
-    // ajax request to log in
-    $('#sign_in').on('click', function () {
+    function sign_in(){
         var username = $('input[name="username"]').val();
         var password = $('input[name="password"]').val();
         $('.login_errors').hide();
@@ -61,6 +45,31 @@ $(document).ready(function () {
                 //alert( "The request is complete!" );
             }
         });
+    }
+
+    //use on sign in button to show login form
+    $('.login').on('click', function () {
+        $('.loginform').fadeIn("slow", "linear");
+        $(this).hide();
+    });
+
+
+    //use on login cancel button
+    $('.login_cancel').on("click", function (e) {
+        //e.preventDefault();
+        $('.login_errors').hide();
+        $('.loginform').fadeOut(500);
+        $('.login').show(1000);
+    });
+
+    // ajax request to log in
+    $('#sign_in').on('click', sign_in);
+
+    // add event when press enter on input field
+    $('#id_password').on('keypress', function(event){
+        if(event.which == 13){
+            sign_in();
+        }
     });
 
     //show loading bar on ajax events
